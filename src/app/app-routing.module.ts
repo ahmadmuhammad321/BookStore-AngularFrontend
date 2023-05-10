5f38c253c8b0d9dd8dcd0fbc0516f3d72ee7c69c
ahmadmuhammad321 <ahmadnwz32@gmail.com>
2023-05-10 18:35:18 +0500

    final

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CategoriesComponent } from './components/categories/categories.component';
+import { AboutPageComponent } from './modules/about-page/about-page.component';
+import { BlogPageComponent } from './modules/blog-page/blog-page.component';
+import { CategoriesPageComponent } from './modules/categories-page/categories-page.component';
+import { ContactPageComponent } from './modules/contact-page/contact-page.component';
+import { MainComponent } from './modules/main/main.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: MainComponent},
+  {path: 'about', component: AboutPageComponent},
+  {path: 'home', component: MainComponent},
+  {path: 'category', component: CategoriesPageComponent},
+  {path: 'blog', component: BlogPageComponent},
+  {path: 'contact', component: ContactPageComponent}
+  
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,36 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainComponent } from './modules/main/main.component';
+import { HeaderComponent } from './components/header/header.component';
+import { SliderComponent } from './components/slider/slider.component';
+import { CategoriesComponent } from './components/categories/categories.component';
+import { AboutComponent } from './components/about/about.component';
+import { ReviewsComponent } from './components/reviews/reviews.component';
+import { BlogsComponent } from './components/blogs/blogs.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { AboutPageComponent } from './modules/about-page/about-page.component';
+import { CategoriesPageComponent } from './modules/categories-page/categories-page.component';
+import { BlogPageComponent } from './modules/blog-page/blog-page.component';
+import { ContactPageComponent } from './modules/contact-page/contact-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainComponent,
+    HeaderComponent,
+    SliderComponent,
+    CategoriesComponent,
+    AboutComponent,
+    ReviewsComponent,
+    BlogsComponent,
+    ContactComponent,
+    FooterComponent,
+    AboutPageComponent,
+    CategoriesPageComponent,
+    BlogPageComponent,
+    ContactPageComponent
   ],
   imports: [
     BrowserModule,
